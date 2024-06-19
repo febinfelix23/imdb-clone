5fbf5ea91607de1e6f3ffdf6fefea7e4782c54cf
febinfelix23 <febinfelix23@gmail.com>
2024-06-19 13:24:19 +0530

    New Update

--- src/components/Sortbar.jsx
+++ src/components/Sortbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import SortbarItems from './SortbarItems'
 
 function Sortbar() {
   return (
     <div className='flex bg-yellow-500 text-black font-semibold p-4 lg:text-lg justify-center gap-10'>
-        <SortbarItems title="Trending" param="fetchTrending"/>
-        <SortbarItems title="Top Rated" param="fetchTopRated"/>
+      <Suspense>
+        <SortbarItems title="Trending" param="fetchTrending" />
+        <SortbarItems title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   )
 }
